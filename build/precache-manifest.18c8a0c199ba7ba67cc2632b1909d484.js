ccfe94e6f5b9772abc7931c7607d1593c7a4a1cc
serjs <sergey.sablin21@gmail.com>
2020-08-09 13:04:01 +0300

    Initialize project using Create React App

---
+++ build/precache-manifest.18c8a0c199ba7ba67cc2632b1909d484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f850602527e862542b22d1d3da1942dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e47b75b182cd6558502d",
+    "url": "/static/css/2.e6f36237.chunk.css"
+  },
+  {
+    "revision": "745af9fba8733063c163",
+    "url": "/static/css/main.1e95929f.chunk.css"
+  },
+  {
+    "revision": "e47b75b182cd6558502d",
+    "url": "/static/js/2.2e7c573a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2e7c573a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745af9fba8733063c163",
+    "url": "/static/js/main.6d3caa07.chunk.js"
+  },
+  {
+    "revision": "88ed8693fc968ec4932e",
+    "url": "/static/js/runtime-main.2dd8316a.js"
+  }
+]);
