9cc4148cfa574d328a00f1bd406733583da7e7fc
serjs <sergey.sablin21@gmail.com>
2020-08-23 01:12:06 +0300

    Updates

---
+++ precache-manifest.45e4b4132a88ad53a636201368224c1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443af95dcd0d956739f0194d9a13d4f6",
+    "url": "/snufkin/index.html"
+  },
+  {
+    "revision": "c6b2606c56378ea309df",
+    "url": "/snufkin/static/css/2.5c600ed6.chunk.css"
+  },
+  {
+    "revision": "7a1c06abdeb730af4886",
+    "url": "/snufkin/static/css/main.00a1fd87.chunk.css"
+  },
+  {
+    "revision": "c6b2606c56378ea309df",
+    "url": "/snufkin/static/js/2.0d63c1a7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/snufkin/static/js/2.0d63c1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1c06abdeb730af4886",
+    "url": "/snufkin/static/js/main.5a4588da.chunk.js"
+  },
+  {
+    "revision": "ff00873fa43f8a60b4b8",
+    "url": "/snufkin/static/js/runtime-main.5ac0fe45.js"
+  },
+  {
+    "revision": "62c7ca2a029366c1c5bb49b9415fd000",
+    "url": "/snufkin/static/media/fa-regular-400.62c7ca2a.ttf"
+  },
+  {
+    "revision": "c1a57903b31e5c40becfce72c0a9ef0f",
+    "url": "/snufkin/static/media/fa-regular-400.c1a57903.woff2"
+  },
+  {
+    "revision": "e0bc0d1d32be09ab0c6b8745e0cc0cf4",
+    "url": "/snufkin/static/media/fa-regular-400.e0bc0d1d.woff"
+  }
+]);
