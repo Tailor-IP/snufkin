b7a651c0874243857da1e588a7a675b0745fbf9f
serjs <sergey.sablin21@gmail.com>
2020-08-31 16:27:57 +0300

    Updates

---
+++ precache-manifest.30178ed66ea4994c324cbfec7e2c6caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b467f1c94f5e16ffacde4ece817cc4a",
+    "url": "/snufkin/index.html"
+  },
+  {
+    "revision": "9174a2fcc208a2f17cba",
+    "url": "/snufkin/static/css/2.5c600ed6.chunk.css"
+  },
+  {
+    "revision": "850f0de4795c9449d88d",
+    "url": "/snufkin/static/css/main.aa29af79.chunk.css"
+  },
+  {
+    "revision": "9174a2fcc208a2f17cba",
+    "url": "/snufkin/static/js/2.32d13145.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/snufkin/static/js/2.32d13145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850f0de4795c9449d88d",
+    "url": "/snufkin/static/js/main.1d90a6d2.chunk.js"
+  },
+  {
+    "revision": "ff00873fa43f8a60b4b8",
+    "url": "/snufkin/static/js/runtime-main.5ac0fe45.js"
+  }
+]);
